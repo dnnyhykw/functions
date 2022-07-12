2007329b44deef895418492c76b77c2722861188
dnnyhykw <68537585+dnnyhykw@users.noreply.github.com>
2022-07-12 19:58:38 +0900

    t

--- src/onestroke.js
+++ src/onestroke.js
@@ -20,30 +20,3 @@ exports.handler = async (event) => {
 		},
 	};
 };
-/*
-exports.handler = async (event) => {
-	// https://dev.to/weaponxii/handling-cors-with-netlify-post-requests-39ld
-	const headers = {
-		"Access-Control-Allow-Origin": "*",
-		"Access-Control-Allow-Headers": "Content-Type",
-		"Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-	};
-	const data = {
-		state: "before",
-		starttime: "",
-	}
-	if (event.httpMethod === "OPTIONS") {
-		return {
-			statusCode: 200,
-			headers,
-			body: JSON.stringify({ message: "Successful preflight call." }),
-		};
-	} else if (event.httpMethod === "POST") {
-		return {
-			statusCode: 200,
-			headers,
-			body: JSON.stringify(data),
-		};
-	}
-};
-*/
